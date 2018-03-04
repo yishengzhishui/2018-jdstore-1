89b581d77ae31af0ca162781f97eeb209a133f00
wangxing <18912540921@163.com>
2018-03-04 20:45:01 +0800

    admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def admin_required
+    redirect_to '/', alert: 'You have no permission' unless current_user.admin?
+  end
 end
