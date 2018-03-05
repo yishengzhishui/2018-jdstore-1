74a00e57e40cadf9987b43ee444601a1628eba29
wangxing <18912540921@163.com>
2018-03-05 20:26:44 +0800

    current_cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,20 @@ class ApplicationController < ActionController::Base
   def admin_required
     redirect_to '/', alert: 'You have no permission' unless current_user.admin?
   end
+
+  # 声明是helper，可以在view中使用
+  helper_method :current_cart
+
+  def current_cart
+    @current_cart ||= find_cart
+  end
+
+  private
+
+  def find_cart
+    cart = Cart.find_by(id: session[:cart_id])
+    cart = Cart.create if cart.blank?
+    session[:cart_id] = cart.id
+    cart
+  end
 end
